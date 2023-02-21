2accf0042a381480626812907c66ef7499813fce
Liliagare <garcia131p@gmail.com>
2023-02-20 19:05:54 -0600

    mi primer commit

---
+++ src/main/java/fundamentosplatzi/fundamentosuno/FundamentosunoApplication.java
@@ -0,0 +1,13 @@
+package fundamentosplatzi.fundamentosuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fundamentosplatzi/fundamentosuno/FundamentosunoApplicationTests.java
@@ -0,0 +1,13 @@
+package fundamentosplatzi.fundamentosuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
